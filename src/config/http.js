ac7032f62e6b6f6b26e4a3a0659456da848bf7d2
lianhai <lianhai1136@126.com>
2018-06-07 07:36:45 +0800

    login page

--- src/config/http.js
+++ src/config/http.js
@@ -1,9 +1,67 @@
 import axios from 'axios';
 import store from 'storejs';
 import router from '../router/index.js'
+import {Alert} from 'iview';
 
 /* axios全局设置 */
-const http = axios.create();
-http.defaults.timeout = 5000;
-http.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8';
+const http = axios.create({
+    timeout: 10000,
+    responseType: "json",
+    withCredentials: false,
+    headers: {
+        "Content-Type": "application/json;charset=UTF-8"
+    }
+});
+
+let IDENTIFICATION = 'schedule_identify';
+
+/* request拦截器 */
+http.interceptors.request.use(
+    config => {
+      if (store.has(IDENTIFICATION)) {
+          // 若token存在则添加request header
+        let identify = JSON.parse(store.get(IDENTIFICATION));
+        config.headers.Authorization = identify.token;
+      }
+
+      return config;
+    },
+    error => {
+        // TODO
+        return Promise.reject(error.data.error.message);
+    }
+  );
+
+/* response拦截器 */
+http.interceptors.response.use(
+    response => {
+      return response.data;
+    },
+    error => {
+        if (!store.has(IDENTIFICATION)) {
+            router.push({
+                path: "/login"
+            });
+        }else{
+            if (error.response.status === 401) {
+                router.push({
+                    path: '/login'
+                });
+            }
+            if (error.response.status === 403) {
+                router.push({
+                    path: '/error/403'
+                });
+            }
+            if (error.response.status === 500) {
+                router.push({
+                    path: "/error/500"
+                });
+            }
+        }
+        let errorInfo =  error.data.error ? error.data.error.message : error.data;
+        return Promise.reject(errorInfo);
+    }
+  );
+
 export default http;
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -5,16 +5,17 @@
             <div class="login">
                 <img src="../assets/img/loginText.png">
                 <form action="login" method="post" id="loginForm">
-                    <p class="error" id="result" style="color:red;"></p>
+                    <!-- <p class="error" id="result" style="color:red;"></p> -->
+                    <Alert id="errorMsg" type="error" style="display:none">{{ errorMsg }}</Alert>
                     <div class="inputBox">
                         <span class="icon-3"></span>
-                        <input type="text"  name="userCode" placeholder="用户名" id="userCode">
+                        <input type="text" v-model.trim="userName" name="userCode" placeholder="用户名" id="userCode">
                     </div>
                     <div class="inputBox">
                         <span class="icon-2"></span>
-                        <input type="password" name="password" placeholder="登录密码" id="password">
+                        <input type="password" v-model.trim="userPasswd" name="password" placeholder="登录密码" id="password">
                     </div>
-                    <router-link :to="{name: 'Index'}" class="btn-login" type="button" >登录</router-link>
+                    <button  class="btn-login" type="button" @click="doLogin">登录</button>
                 </form>
             </div>
         </div>
@@ -23,23 +24,51 @@
 <script>
     import * as api from "../api/commonAPI";
     export default{
-    }
-    function submitLogin(){
-        var name = $("#userCode").val();
-        var pass = $("#password").val();
-        if($.trim(name)=='' && $.trim(pass)==''){
-            $("#result").text("请输入用户名和密码");
-            return;
+        data: function () {
+            return {
+                userName: '',
+                userPasswd: '',
+                errorMsg: ''
+            };
+        },
+        methods: {
+            doLogin: async function (){
+                if(this.userName=='' && this.userPasswd==''){
+                    this.errorMsg = '账号和密码不能为空';
+                    $('#errorMsg').css('display','block');
+                    return;
+                }
+                if(this.userName=='' && !this.userPasswd==''){
+                    this.errorMsg = '账号不能为空';
+                    $('#errorMsg').css('display','block');
+                    return;
+                }
+                if(!this.userName=='' && this.userPasswd==''){
+                    this.errorMsg = '密码不能为空';
+                    $('#errorMsg').css('display','block');
+                    return;
+                }
+                // 若用户名为admin密码为123456则允许登录
+                // if(this.userName === 'admin' && this.userPasswd === '123456'){
+                    let params = {
+                        userName: this.userName,
+                        password: this.userPasswd
+                    };
+                    this.$Loading.start();
+                    let response = await api.login(params);
+                    if (response.meta.code !== 0) {
+                        this.$Loading.error();
+                        this.$Message.error(response.meta.message);
+                    }else{
+                        this.$Loading.finish();
+                        this.$router.push({ path: "/home" });
+                    }
+                // }else{
+                //     this.errorMsg = '账号或密码错误';
+                //     $('#errorMsg').css('display','block');
+                // }
+            }
         }
-        if($.trim(name)=='' && !$.trim(pass)==''){
-            $("#result").text("请输入用户名");
-            return;
-        }
-        if(!$.trim(name)=='' && $.trim(pass)==''){
-            $("#result").text("请输入密码");
-            return;
-        }
-        $("#loginForm").submit();
     }
 </script>
 <style>
