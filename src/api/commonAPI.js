b86b4caf344dd0483b29a2f0cbee4f3def2a7d24
jsychen <jsychen@qq.com>
2018-07-11 15:46:10 +0800

    00

--- src/api/commonAPI.js
+++ src/api/commonAPI.js
@@ -44,6 +44,9 @@ export const getRole = () => fetch('/role', {},'GET');
 /** 获取用户列表 */
 export const getUser = data => fetch('/user', data ,'GET');
 
+/** 获取备班用户列表 */  
+export const getBackupUser = districtId => fetch('/user/backuplist/' + districtId, {} ,'GET');
+
 /** 修改用户 */
 export const updateUser = data => fetch('/user', data, 'PUT');
 
