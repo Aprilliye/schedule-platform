0680477eaec155635391d7e7e5a8ede53ca1b4b4
lianhai <lianhai1136@126.com>
2018-07-17 14:00:16 +0800

    dowoload

--- src/config/fetch.js
+++ src/config/fetch.js
@@ -9,6 +9,19 @@ const timeoutResponse = {
     }
 };
 
+function download (data) {
+    if (!data) {
+        return
+    }
+    let url = window.URL.createObjectURL(new Blob([data]))
+    let link = document.createElement('a')
+    link.style.display = 'none'
+    link.href = url
+    link.setAttribute('download', 'schedule.zip')
+    document.body.appendChild(link)
+    link.click()
+}
+
 export default async (url = '', data = {}, method = 'GET') => {
     method = method.toUpperCase();
     url = baseURL + url;
@@ -23,11 +36,22 @@ export default async (url = '', data = {}, method = 'GET') => {
             params = params.substr(0, params.lastIndexOf('&'));
             url = url + '?' + params;
         }
-        return await http.get(url)
+        if(url.indexOf('/schedule/scheduleinfo/export/img')> -1){
+            return await http({
+                method: 'get',
+                url: url,
+                responseType: 'blob'
+            }).then(response => {
+                download(response);
+            }).catch((error) => {
+            });
+        }else{
+            return await http.get(url)
             .then(response => response)
             .catch(function (err) {
                 return timeoutResponse;
             });
+        }
     }
 
     if (method === 'POST' || method === 'PUT') {
