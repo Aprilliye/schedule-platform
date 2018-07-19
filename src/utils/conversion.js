4c922c3785742fdf51449ada8a7988da77862ec4
jsychen <jsychen@qq.com>
2018-07-19 16:16:56 +0800

    33

--- src/utils/conversion.js
+++ src/utils/conversion.js
@@ -1,5 +1,9 @@
 export default function (date) {
-    let month = (date.getMonth()+1) < 10 ? ('0' + (date.getMonth()+1)) : (date.getMonth()+1);
+    if(!date){
+        return;
+    }
+    let month = date.getMonth() + 1;
+    month = month < 10 ? ('0' + month) : month;
     let day = date.getDate() < 10 ? ('0' + date.getDate()) : date.getDate();
     let dateStr = date.getFullYear() + '-' + month  + '-' + day;
     return dateStr;
